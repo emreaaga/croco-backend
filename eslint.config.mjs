431f6f88daaed6f89f6bb99af6c553fab7a25ff3
Emre Agaliev <emreagaliev@gmail.com>
2025-10-15 12:19:46 +0300

    eslint & prettier init

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import js from "@eslint/js";
+import globals from "globals";
+import prettier from "eslint-plugin-prettier";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+      },
+    },
+    plugins: {
+      prettier, 
+    },
+    rules: {
+      ...js.configs.recommended.rules, 
+      "prettier/prettier": "warn", 
+      "no-unused-vars": "warn",
+      "no-console": "off",
+    },
+  },
+]);
