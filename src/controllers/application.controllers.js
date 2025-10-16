f72f47f622af30733c9ee4a28954b612974db991
Emre Agaliev <emreagaliev@gmail.com>
2025-10-16 12:21:46 +0300

    added pagination for app route

--- src/controllers/application.controllers.js
+++ src/controllers/application.controllers.js
@@ -1,4 +1,4 @@
-import { createApplication } from '../models/application.model.js';
+import { createApplication, getApplications } from '../models/application.model.js';
 
 export const createApplicationController = async (request, response) => {
   try {
@@ -13,6 +13,28 @@ export const createApplicationController = async (request, response) => {
     return response.status(500).json({
       success: false,
       message: 'Server error',
+    });
+  }
+};
+
+export const getApplicationsController = async (request, response) => {
+  try {
+    let page = parseInt(request.query.page || 1);
+    let page_size = parseInt(request.query.page_size || 10);
+    if (page < 1) page = 1;
+    if (page_size < 1) page_size = 10;
+
+    const { data, pagination } = await getApplications(page, page_size);
+    return response.status(200).json({
+      success: true,
+      data,
+      pagination,
+    });
+  } catch (error) {
+    console.log(error);
+    return response.status(500).json({
+      success: false,
+      message: 'Server error.',
     });
   }
 };
--- src/routes/application.routes.js
+++ src/routes/application.routes.js
@@ -1,10 +1,15 @@
 import { Router } from 'express';
-import { createApplicationController } from '../controllers/application.controllers.js';
+import {
+  createApplicationController,
+  getApplicationsController,
+} from '../controllers/application.controllers.js';
 import { ApplicationSchema } from '../validations/application.validations.js';
 import { handleValidate } from '../middlewares/handleValidate.js';
+import { authMe } from '../middlewares/authMe.js';
 
 const router = Router();
 
 router.post('/', handleValidate(ApplicationSchema), createApplicationController);
+router.get('/', authMe, getApplicationsController);
 
 export default router;
