5668a6b55e1c94f7aa33377778dc603cd16b927d
Emre Agaliev <emreagaliev@gmail.com>
2025-10-21 13:21:14 +0300

    added check id

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -5,7 +5,11 @@ export const authMiddleware = async (request, response, next) => {
   if (!token) return response.status(401).json({ message: 'Not authenticated.' });
 
   try {
-    jwt.verify(token, 'process.env.JWT_SECRET');
+    const encoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (!encoded?.id) {
+      return response.status(401).json({ success: false, message: 'Invalid token payload' });
+    }
+    request.userId = encoded.id;
     next();
   } catch (error) {
     console.error('JWT error', error.message);
