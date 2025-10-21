0fded2644ed8b62a229c23bcbc6930c33d2aa106
Emre Agaliev <emreagaliev@gmail.com>
2025-10-21 18:04:41 +0300

    disable eslint in building

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -9,6 +9,10 @@ const nextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
   async redirects() {
     return [
       {
