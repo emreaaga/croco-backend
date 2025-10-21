55bbb794f90839fe581bb6fc07b83f3d9013148d
Emre Agaliev <emreagaliev@gmail.com>
2025-10-21 14:49:17 +0300

    added changeUserStatusController

--- src/controllers/user.controolers.js
+++ src/controllers/user.controolers.js
@@ -1,4 +1,4 @@
-import { getUsers } from '../models/user.model.js';
+import { getUsers, changeUserStatus } from '../models/user.model.js';
 
 export const getUsersController = async (request, response) => {
   try {
@@ -16,5 +16,32 @@ export const getUsersController = async (request, response) => {
       success: false,
       message: 'Server error.',
     });
+  }
+};
+
+export const changeUserStatusController = async (request, response) => {
+  try {
+    const user_status = request?.validatedData?.status;
+    const user_id = Number(request.params?.id);
+    if (!user_status || !user_id) {
+      return response.status(400).json({ message: 'User status or ID not provided.' });
+    }
+
+    const allowedStatuses = ['approved', 'rejected'];
+    if (!allowedStatuses.includes(user_status)) {
+      return response.status(400).json({ success: false, message: 'Invalid user status value.' });
+    }
+
+    const [result] = await changeUserStatus(user_id, user_status);
+    if (!result) {
+      return response.status(404).json({ success: false, message: 'User not found.' });
+    }
+
+    return response
+      .status(200)
+      .json({ success: true, message: 'User status updated successfully.', data: result });
+  } catch (error) {
+    console.log(error);
+    return response.status(500).json({ success: false, message: 'Server error.' });
   }
 };
