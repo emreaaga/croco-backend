ed43daa957e395de97dbdcf4adadfcf352332db0
Emre Agaliev <emreagaliev@gmail.com>
2025-10-21 14:50:28 +0300

    added patch rout for change status

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { getUsersController } from '../controllers/user.controolers.js';
+import { getUsersController, changeUserStatusController } from '../controllers/user.controolers.js';
 import { authMiddleware } from '../middlewares/authMiddleware.js';
 import PaginateValidation from '../validations/query.validations.js';
+import UserPatchSchema from '../validations/user.validations.js';
 import { handleValidate } from '../middlewares/handleValidate.js';
 
 const router = Router();
 router.get('/', authMiddleware, handleValidate(PaginateValidation, true), getUsersController);
+router.patch('/:id', authMiddleware, handleValidate(UserPatchSchema), changeUserStatusController);
 
 export default router;
