0dd7ec6b6355ef28ac9d4392ed18fc54950fa15d
Emre Agaliev <emreagaliev@gmail.com>
2025-10-29 13:42:05 +0300

    added cookie utils f controllers

---
+++ src/utils/cookie.js
@@ -0,0 +1,26 @@
+const isProd = process.env.NODE_ENV === 'production';
+
+const cookieOptions = {
+  access: {
+    httpOnly: true,
+    maxAge: 15 * 60 * 1000,
+    path: '/',
+    domain: isProd ? '.crocodile-pay.uz' : undefined,
+    secure: isProd,
+    sameSite: isProd ? 'none' : 'lax',
+  },
+  refresh: {
+    httpOnly: true,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    path: '/',
+    domain: isProd ? '.crocodile-pay.uz' : undefined,
+    secure: isProd,
+    sameSite: isProd ? 'none' : 'lax',
+  },
+};
+
+export const setAuthCookies = (response, accessToken, refreshToken) => {
+  response.cookie('access_token', accessToken, cookieOptions.access);
+  response.cookie('refresh_token', refreshToken, cookieOptions.refresh);
+};
+export const clearAuthCookies = response => {};
---
+++ src/utils/index.js
@@ -0,0 +1,1 @@
+export { setAuthCookies } from './cookie.js';
