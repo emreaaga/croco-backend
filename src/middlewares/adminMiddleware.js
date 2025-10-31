37ed566db36ba81dfebc02ffbfc8fc27ca1fcbee
Emre Agaliev <emreagaliev@gmail.com>
2025-10-31 12:13:46 +0300

    use appError utils

--- src/middlewares/adminMiddleware.js
+++ src/middlewares/adminMiddleware.js
@@ -1,20 +1,16 @@
+import { UnauthorizedError, ForbiddenError } from '../utils/index.js';
+
 export const isAdminMiddleware = async (request, response, next) => {
-  try {
-    const userRole = request.userRole;
+  const userRole = request.userRole;
 
-    if (!userRole) {
-      return response.status(401).json({ success: false, message: 'Unauthorized: missing role.' });
-    }
-
-    const allowed = ['admin'];
-
-    if (!allowed.includes(userRole)) {
-      return response.status(403).json({ success: false, message: 'Access denied.' });
-    }
-
-    next();
-  } catch (error) {
-    console.log(error);
-    return response.status(500).json({ success: false, message: 'Server error' });
+  if (!userRole) {
+    throw new UnauthorizedError('Unauthorized: missing role.');
   }
+
+  const allowed = ['admin'];
+  if (!allowed.includes(userRole)) {
+    throw new ForbiddenError('Access denied.');
+  }
+
+  next();
 };
